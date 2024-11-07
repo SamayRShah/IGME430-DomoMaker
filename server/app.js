e097700139eaaf045e058b9b4b5ec45f212aa1bc
SamayRShah <srs6812@rit.edu>
2024-11-07 00:22:54 -0500

    Finished Domo maker A

---
+++ server/app.js
@@ -0,0 +1,43 @@
+// imports
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+// router
+const router = require('./router.js');
+
+// hosting port
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// connect to mongo
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI).then((instance) => {
+  console.log(`Connected to mongo instance: ${instance.version}`);
+}).catch((err) => {
+  console.log('Could not connect to database');
+  throw err;
+});
+
+// routes
+const app = express();
+
+app.use(helmet());
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(express.json());
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) { throw err; }
+  console.log(`Listening on port: ${port}`);
+});
